30fda1bcaccd95dcfa551e03b59ec700e8d0dde8
bdurb <brian@rpmgreenville.com>
2018-02-16 12:43:34 -0500

    testing get call to api, successfully recieving target value

--- src/app.js
+++ src/app.js
@@ -10,6 +10,18 @@ const STATUS_SUCCESS = 200;
 
 server.use(bodyParser.json());
 
+server.get('/compare', (req,res) => {
+  fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(value => value.json())
+    .then(value => {
+      res.status(STATUS_SUCCESS);
+      res.send(value.bpi.USD.rate);
+    })
+    .catch(err => {
+      res.status(USER_ERROR);
+      res.send({err: err})
+    });
+});
 
 server.listen(PORT, () => {
   console.log(`Server listening on port:${PORT}`)
